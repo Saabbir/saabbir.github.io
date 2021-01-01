1b289c021687faa13a31e93116109ea49f7f02b0
Saabbir Hossain <thesaabbir@gmail.com>
2021-01-01 10:52:38 +0600

    Add drift chat plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,10 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    {
+      src: '~plugins/drift.js',
+      mode: 'client'
+    }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
