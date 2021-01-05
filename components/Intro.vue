36b8434ba1b86eb9e80ad2d3bb71c4cd9ab925f0
Saabbir Hossain <thesaabbir@gmail.com>
2021-01-05 08:49:38 +0600

    Fix intro typo

--- components/Intro.vue
+++ components/Intro.vue
@@ -6,7 +6,7 @@
     <div class="c-intro__content">
       <h1>Hi, I'm Saabbir.</h1>
       <p>
-        A <strong>front-end web developer</strong> from Bangladesh, currently working as a independent contractor at <a class="link" href="https://www.upwork.com/o/profiles/users/_~0125f9d541d9412fba/" target="_blank">Upwork</a>. I build static sites and apps using <strong>Vuejs</strong> and <strong>Nuxtjs</strong>.
+        A <strong>front-end web developer</strong> from Bangladesh, currently working as an independent contractor at <a class="link" href="https://www.upwork.com/o/profiles/users/_~0125f9d541d9412fba/" target="_blank">Upwork</a>. I build static sites and apps using <strong>Vuejs</strong> and <strong>Nuxtjs</strong>.
       </p>
       <p>
         I've passion building awesome things on the web. I love everything about design and development but my skills place me at the intersection of both.
