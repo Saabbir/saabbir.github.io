83741300559cbe0c5d3e2deb8bf4e3b8ad51a413
Saabbir Hossain <thesaabbir@gmail.com>
2021-01-12 18:52:20 +0600

    Update intro copy

--- components/Intro.vue
+++ components/Intro.vue
@@ -8,9 +8,10 @@
       <p>
         A <strong>front-end web developer</strong> based in Barisal, Bangladesh, currently working as an independent contractor at <a class="link" href="https://www.upwork.com/o/profiles/users/_~0125f9d541d9412fba/" target="_blank">Upwork</a>. I build static sites and apps using <strong>Vuejs</strong> and <strong>Nuxtjs</strong>.
       </p>
-      <p>
+      <!-- <p>
         I've passion building awesome things on the web. I love everything about design and development but my skills place me at the intersection of both.
-      </p>
+      </p> -->
+      <p>I'm <strong>open to work</strong> for a <strong>full-time position</strong> where I can work as a creative front-end developer.</p>
       <div class="c-buttons u-mt-48">
         <!-- <a href="mailto:thesaabbir@gmail.com" class="c-button">Get in touch</a> -->
         <nuxt-link to="/work" class="c-button c-button--lg">View featured works</nuxt-link>
