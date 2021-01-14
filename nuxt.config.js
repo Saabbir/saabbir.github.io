39b2b3a48f09e713d4cbc0ea2c2084d970ce23a4
Saabbir Hossain <thesaabbir@gmail.com>
2021-01-14 12:55:57 +0600

    Disable drift plugin for now

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,10 +57,10 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    {
-      src: '~plugins/drift.js',
-      mode: 'client'
-    }
+    // {
+    //   src: '~plugins/drift.js',
+    //   mode: 'client'
+    // }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
