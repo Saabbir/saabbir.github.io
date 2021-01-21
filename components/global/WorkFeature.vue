d1968477d56fb79a2b33741b329298e5ff6d53fa
Saabbir Hossain <thesaabbir@gmail.com>
2021-01-21 22:41:12 +0600

    Use svg as image

--- components/global/WorkFeature.vue
+++ components/global/WorkFeature.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="c-feature-figure">
     <div class="c-feature-figure__img-container">
-      <!-- <img :src="require(`~/assets/images/${src}`)" :alt="alt"> -->
-      <div class="c-feature-figure__svg-container" v-html="require(`~/assets/images/${src}?include`)"></div>
+      <img :src="require(`~/assets/images/${src}`)" :alt="alt">
+      <!-- <div class="c-feature-figure__svg-container" v-html="require(`~/assets/images/${src}?include`)"></div> -->
     </div>
     <div class="c-feature-figure__caption">
       <slot></slot>
