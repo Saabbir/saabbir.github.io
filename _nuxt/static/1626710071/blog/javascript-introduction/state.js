13dd52d0adf77aeae237742f19eb8f7ceaaf52a6
Saabbir <Saabbir@users.noreply.github.com>
2021-07-19 15:54:34 +0000

    deploy: bb31999538fc4a8410e298a8930fb1cab428171f

---
+++ _nuxt/static/1626710071/blog/javascript-introduction/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626710071",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fjavascript-introduction",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"88be9427"}}}}(null));
