76bbbcab962979a4e63ccb09aedaa3ac7831e81a
Saabbir Hossain <thesaabbir@gmail.com>
2021-07-26 13:29:00 +0600

    Fix pagination condition

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="c-pagination">
-    <div class="c-pagination__prev">
-      <div v-if="prev" class="c-pagination__link-label">Previous {{type}}</div>
+    <div class="c-pagination__prev" v-if="prev">
+      <div class="c-pagination__link-label">Previous {{type}}</div>
       <NuxtLink
-        v-if="prev"
         :to="{ name: routeName, params: { slug: prev.slug } }"
         class="c-button c-button--lg"
       >
         &laquo; {{ prev.title }}
       </NuxtLink>      
     </div>
-    <div class="c-pagination__next">
-      <div v-if="next" class="c-pagination__link-label">Next {{type}}</div>
+    <div class="c-pagination__next" v-if="next">
+      <div class="c-pagination__link-label">Next {{type}}</div>
       <NuxtLink
-        v-if="next"
         :to="{ name: routeName, params: { slug: next.slug } }"
         class="c-button c-button--lg"
       >
