ef9143b89df89ca433abf0f8ddf248b66b10142b
Saabbir <Saabbir@users.noreply.github.com>
2021-07-28 06:40:31 +0000

    deploy: b3f043072cc5ab5e7ced3fe187a5c26b62a245b3

---
+++ _nuxt/static/1627454427/blog/tag/theory/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627454427",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002Ftheory",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"324da21e"}}}}(null));
