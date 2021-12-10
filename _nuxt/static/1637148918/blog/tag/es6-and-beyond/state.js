902bd4dc20295235e612990c134072c469ec14e6
Saabbir <Saabbir@users.noreply.github.com>
2021-12-10 07:35:35 +0000

    deploy: ef2ca6a9f712e075747afae37e4ab0928dd8749e

--- _nuxt/static/1637148918/blog/tag/es6-and-beyond/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637148918",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002Fes6-and-beyond",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a7a3f3c1"}}}}(null));
