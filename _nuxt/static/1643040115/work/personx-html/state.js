28e27644827f320d9384e9b636f3d5a2bcab0bd7
Saabbir <Saabbir@users.noreply.github.com>
2022-01-24 16:01:58 +0000

    deploy: 60e26a758c2e926256d90e2cdee45261d0ad58f9

---
+++ _nuxt/static/1643040115/work/personx-html/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643040115",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwork\u002Fpersonx-html",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c61cafe2"}}}}(null));
