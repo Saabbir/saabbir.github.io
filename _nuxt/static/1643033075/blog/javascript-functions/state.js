f38749b6c177b50a770de4b9a2c9fe0383675e81
Saabbir <Saabbir@users.noreply.github.com>
2022-01-24 14:16:08 +0000

    deploy: f4f1854fb6ab6f679cc123901af6b49f418f4302

--- _nuxt/static/1643033075/blog/javascript-functions/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643033075",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fjavascript-functions",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"977321c2"}}}}(null));
--- _nuxt/static/1643033075/snippets/tag/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643033075",layout:"default",error:a,serverRendered:false,routePath:"\u002Fsnippets\u002Ftag",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"977321c2"}}}}(null));
