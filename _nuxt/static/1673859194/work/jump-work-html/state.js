d6e037c725e3dbecc2f80252ed62ee58e0a40fac
Saabbir <Saabbir@users.noreply.github.com>
2023-01-17 14:43:38 +0000

    deploy: 9802a4b070909f5199aad02276b89f10998d6d16

--- _nuxt/static/1673859194/work/jump-work-html/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673859194",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwork\u002Fjump-work-html",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5482240f"}}}}(null));
