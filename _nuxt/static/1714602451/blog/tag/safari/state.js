634df45391396ff59263aa2b4b0dfc6f7342363a
Saabbir <Saabbir@users.noreply.github.com>
2024-05-01 22:41:05 +0000

    deploy: cb4ed4ed12309d8e896540f1a1554e2de3f9ffa9

--- _nuxt/static/1714602451/blog/tag/safari/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1714602451",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002Fsafari",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"09bd04b7"}}}}(null));
