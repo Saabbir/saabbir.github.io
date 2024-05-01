84baf255ae2939e6914bf6582f605f0fb4f49448
Saabbir <Saabbir@users.noreply.github.com>
2024-05-01 20:20:30 +0000

    deploy: d3d257e49dc15ad679ea3309b5c4cfeaad576d8e

---
+++ _nuxt/static/1714594827/work/cesppa-prototype/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1714594827",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwork\u002Fcesppa-prototype",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"00c7649e"}}}}(null));
