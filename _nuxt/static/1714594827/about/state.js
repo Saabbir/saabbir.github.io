bd4a28d1204085b1b47077490a4d6e654119a41f
Saabbir <Saabbir@users.noreply.github.com>
2024-05-01 22:20:01 +0000

    deploy: 359cc58accec04e625c79fd94bb94580b5251565

--- _nuxt/static/1714594827/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1714594827",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"00c7649e"}}}}(null));
---
+++ _nuxt/static/1714601999/work/cesppa-redesign-v2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1714601999",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwork\u002Fcesppa-redesign-v2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0069010a"}}}}(null));
