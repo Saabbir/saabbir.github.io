4598960fcf828ecb4a32dcf873f9bb9c1a300267
Saabbir <Saabbir@users.noreply.github.com>
2025-08-20 19:44:04 +0000

    deploy: cc33cc770c41320f363f6a31fd0edaaf46a6775f

--- _nuxt/static/1717099350/blog/tag/fix/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1717099350",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002Ffix",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f9b8449d"}}}}(null));
--- _nuxt/static/1717099350/snippets/style-placeholder-text/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1717099350",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsnippets\u002Fstyle-placeholder-text",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f9b8449d"}}}}(null));
