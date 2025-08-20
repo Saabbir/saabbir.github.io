45bc74f10e85a500381eb9234218e01892ab3916
Saabbir <Saabbir@users.noreply.github.com>
2025-08-20 19:55:22 +0000

    deploy: 6dbe9e8ba866388f2c60b31d6c189e7068087ca2

---
+++ _nuxt/static/1755719719/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1755719719",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"523193ce"}}}}(null,"\u002F"));
